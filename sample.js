993e4067f1e6966c4aba6b607f798f8247b09cc3
fzcs <fzcs@live.cn>
2014-06-18 16:24:53 +0800

    no message

--- sample.js
+++ sample.js
@@ -6,16 +6,24 @@ var mmseg = require('./lib/mmseg');
 var txt = "施乃康说，（一）我以我的视觉与感悟，（二）告诉来与我聊天的华人华侨和海外施氏宗亲们，（三）告诉他们为了中华民族的复兴和中国人在世界上有尊严，习近平和他的同事们工作的很累、很辛苦。我们必须坚定的支持习近平和他的同事们，支持中国共产党一心为民族复兴的事业。";
 //var res = m.parse([["インデックス一括,ＡdA作成する.sadfとき、十分に注意する必要のある作成機能", 10], ["李林", 1]]);
 
-mmseg.segStr(txt, function (err, result) {
-  console.log(result);
-  console.log("-------------------------------------");
-  mmseg.extendDic(['（一）', '（二）'], function (err) {
-    if (err) console.log(err);
-    mmseg.segStr(txt, function (err, result) {
-      console.log(result);
-    });
+
+mmseg.loadBlacklist(function (err, blacklist) {
+  console.log(blacklist);
+  mmseg.segStr(txt, function (err, result) {
+    console.log(result);
+    console.log("-------------------------------------");
   });
 });
+//mmseg.segStr(txt, function (err, result) {
+//  console.log(result);
+//  console.log("-------------------------------------");
+//  mmseg.extendDic(['（一）', '（二）'], function (err) {
+//    if (err) console.log(err);
+//    mmseg.segStr(txt, function (err, result) {
+//      console.log(result);
+//    });
+//  });
+//});
 
 
 //s.append("LawyerOnline", "test"
