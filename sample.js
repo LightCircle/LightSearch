0566142f7bbd705476b699f422b0d6f8a70d279e
fzcs <fzcs@live.cn>
2014-06-22 20:22:33 +0800

    C++ 版本的 ictclas 分词实现

--- sample.js
+++ sample.js
@@ -8,11 +8,42 @@ var txt = "施乃康说，（一）我以我的视觉与感悟，（二）告诉
 //  console.log(result);
 //});
 //var res = m.parse([["インデックス一括,ＡdA作成する.sadfとき、十分に注意する必要のある作成機能", 10], ["李林", 1]]);
-ictclas.doExtend('/opt/LightSearch/ictclas/userdic.txt',function(result){
+
+
+/*
+* !!!目前ictclas只支持64位 linux 系统,其他系统均不支持!!!
+* */
+
+ /*
+* doExtend
+* 用来扩展分词词典
+* 参数:
+*   为自定义词典的绝对路径,自定义词典里的词词性识别码为g.
+* 返回值:
+*   加载的自定义词的个数(由于对 C++ 不熟,所以返回值是字符串类型的数字.)
+* 注意点:
+*   1.可能是由于 ictclas 的 bug,自定义词典文件的第一行无法被识别,所以第一行需要留空.
+*   2.除第一行以外,其他行每一行写一个词,格式为 "词语@@g".
+*   3.自定义词典的导入是全局的.比如说第一次导入 A B 两个词,第二次导入只导入 A 词,
+*     则第二次导入后分词系统不会识别出 B 这个词.
+*
+* */
+
+/*
+ * doWork
+ * 用来分词
+ * 参数:
+ *   需要分词的内容,必须为 UTF-8 编码.
+ * 返回值:
+ *   未切分的分词结果.
+ *
+ * */
+
+ ictclas.doExtend('/opt/LightSearch/ictclas/userdic.txt', function (result) {
+  console.log(result);
+  ictclas.doWork(txt, function (result) {
     console.log(result);
-ictclas.doWork(txt,function(result){
-    console.log(result);
-});
+  });
 });
 //ictclas.doWork(txt,function(result){
 //    console.log(result);
